b82308d02ff4488e7102cfe1a1b7bf7c3070ad98
ffrangu <fitonfrangi@gmail.com>
2020-12-27 16:29:11 +0100

    Add project files.

---
+++ Algorithm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Horspool_s_Algorithm
+{
+    public class Algorithm
+    {
+        public long[] ShiftTable(string pattern)
+        {
+            var patternToArray = pattern.ToCharArray();
+
+            var patternASCII = Encoding.ASCII.GetBytes(pattern);
+            long patternLength = patternASCII.LongLength;
+
+            var table = new long[256];
+
+            for (int i = 0; i < 255; i++)
+            {
+                table[i] = patternLength;
+
+                for (int j = 0; j < patternLength - 2; j++)
+                {
+                    table[patternASCII[j]] = patternLength - 1 - j;
+                }
+            }
+
+            return table;
+        }
+
+        public long Horspool(string text, string pattern)
+        {
+            var textASCII = Encoding.ASCII.GetBytes(text);
+            var patternASCII = Encoding.ASCII.GetBytes(pattern);
+
+            long textLength = textASCII.LongLength;
+            long patternLength = patternASCII.LongLength;
+
+            long[] shift_table = ShiftTable(pattern);
+
+            long index = patternLength - 1;
+            while(index <= textLength - 1)
+            {
+                int k = 0;
+
+                while(k <= patternLength - 1 && patternASCII[patternLength - 1 - k] == textASCII[index - k])
+                {
+                    k = k + 1;
+                }
+
+                if (k == patternLength)
+                    return index - patternLength + 1;
+                else
+                    index = index + shift_table[textASCII[index]];
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Horspool_s_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Algorithm algoritmi = new Algorithm();
+
+            long foundIndex1 = algoritmi.Horspool("lorem Ipsum is simply dummy text of the printing and typesetting industry", "industry");
+            long foundIndex2 = algoritmi.Horspool("lorem Ipsum is simply dummy text of the printing and typesetting industry", "dummy");
+            long foundIndex3 = algoritmi.Horspool("lorem Ipsum is simply dummy text of the printing and typesetting industry", "simply");
+            long foundIndex4 = algoritmi.Horspool("lorem Ipsum is simply dummy text of the printing and typesetting industry", "ply");
+            long foundIndex5 = algoritmi.Horspool("lorem Ipsum is simply dummy text of the printing and typesetting industry", "stry");
+
+            Console.WriteLine("Patterni i pare eshte bere match ne indeksin " + foundIndex1 + Environment.NewLine 
+                + "Patterni i dyte eshte bere match ne indeksin " + foundIndex2 + Environment.NewLine
+                + "Patterni i trete eshte bere match ne indeksin " + foundIndex3 + Environment.NewLine
+                + "Patterni i katert eshte bere match ne indeksin " + foundIndex4 + Environment.NewLine
+                +"Patterni i peste eshte bere match ne indeksin " + foundIndex5 + Environment.NewLine);
+        }
+    }
+}
